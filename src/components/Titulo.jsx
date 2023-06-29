4ba952fef6fcac87c02d1ed7d729ddb626be8974
raulenzo <raru69@hotmail.com>
2023-06-29 15:15:11 +0200

    prueba en la rama

---
+++ src/components/Titulo.jsx
@@ -0,0 +1,14 @@
+const Titulo = () =>{
+console.log("hola, mundo") 
+
+return(
+   
+        <div>
+        <h1>hola soy un componente de react</h1>
+        <p>hola como estas</p>
+            </div>
+        )
+        
+}
+
+export default Titulo;

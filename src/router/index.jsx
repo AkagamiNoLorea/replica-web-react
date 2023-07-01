2937cf2f30eae043cd9d3b90ce06af4fa9793ee6
AkagamiNoLorea <raiseta92@gmail.com>
2023-07-01 21:48:48 +0200

    añadidos children y canvio de rama a arreglar errores

--- src/router/index.jsx
+++ src/router/index.jsx
@@ -3,21 +3,29 @@ import Home from "../pages/Home";
 import Extra1 from "../pages/Extra1";
 import Extra2 from "../pages/Extra2";
 import Extra3 from "../pages/extra3";
+import Layout from "../components/Layout/Layout";
 
 export const router = createBrowserRouter([
     {
         path: "/",
-        element: <Home />,
-    },
-    {
-        path: "/articledetails",
-        element: <Extra1 />,
-    },
-    {
-        path: "/termscomditions",
-        element: <Extra2 />,
-    },{
-        path: "/privacypolicy",
-        element: <Extra3 />,
-    },
+        element: <Layout />,
+        children:
+        [
+            {
+                path: "/",
+                element: <Home />,
+            },
+            {
+                path: "/articledetails",
+                element: <Extra1 />,
+            },
+            {
+                path: "/termscomditions",
+                element: <Extra2 />,
+            }, {
+                path: "/privacypolicy",
+                element: <Extra3 />,
+            },
+        ]
+    }
 ]);

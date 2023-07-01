a17f4b1fb73b633e44b2163dd7514f39487a8b9f
AkagamiNoLorea <raiseta92@gmail.com>
2023-07-01 20:37:46 +0200

    elementos creados, falta el css

--- src/components/moleculas/Logo.jsx
+++ src/components/moleculas/Logo.jsx
@@ -2,7 +2,7 @@
 const Logo = () => {
     return (
         <div>
-            <a href="./index.html"><img className="logo" src="./images/logo.svg" alt="Logo Empresa"></a>
+            <a href="./index.html"><img className="logo" src="..../src/components/images/logo.svg" alt="Logo Empresa"></a>
         </div>
     )
 }
---
+++ src/components/plantillas/Navbar.jsx
@@ -0,0 +1,13 @@
+import Logo from "../moleculas/Logo"
+import Listanavbar from "../moleculas/Listanavbar"
+
+const Navbar = () => {
+  return (
+    <div>
+        <Logo/>
+        <Listanavbar/>
+    </div>
+  )
+}
+
+export default Navbar

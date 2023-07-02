0237f772dc57e1cd32fd54fb0c4f847ccf8d581d
Albert <alabertus@gmail.com>
2023-07-02 13:17:38 +0200

    titulo

--- src/components/Titulo.jsx
+++ src/components/Titulo.jsx
@@ -1,14 +1,16 @@
-const Titulo = () =>{
-console.log("hola, mundo") 
+const Titulo = () => {
+    console.log("Mobile App")
 
-return(
-   
+    return (
+
         <div>
-        <h1>hola soy un componente de react</h1>
-        <p>hola como estas</p>
-            </div>
-        )
-        
+            <h1>Mobile App Landing Page</h1>
+            <p>Star focusing on your goalsand get more things done with Sync mobile application. It is
+                the first app to harness the power of social connections to help you stay focused and get
+                organized</p>
+        </div>
+    )
+
 }
 
 export default Titulo;

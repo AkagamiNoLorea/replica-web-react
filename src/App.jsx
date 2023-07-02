ea7db08919caad8ddf5104bfa02e51d8fd4bdaf7
Albert <alabertus@gmail.com>
2023-07-02 23:15:13 +0200

    App

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import Circuloamarillo from './components/Circuloamarillo';
+import Circuloazul from './components/Circuloazul';
+import Rombo from './components/Rombo';
+import Titulo from './components/Titulo';
+
+  
+ 
+function App() {
+  return (
+    <>
+    <Circuloamarillo />
+    <Circuloazul />
+    <Rombo />
+    <Titulo />
+    </>
+  );
+
+}
+
+export default App;

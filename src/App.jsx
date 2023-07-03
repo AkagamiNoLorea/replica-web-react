4d602d8d76fd011941f3ed26525d49fc31ae80e4
raulenzo <raru69@hotmail.com>
2023-07-03 22:23:23 +0200

    atomos

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 
-import Parrafo from './components/Parrafo';
-import Parrafo1 from './components/Parrafo1';
-import Botons from './components/Botons';
-import Image from './components/Image';
-import List from './components/List';
-import Image1 from './components/Image1';
-import Organize from './components/Organize';
-import Imagenes2 from './components/Imagenes2';
+import Parrafo from './components/atomos/Parrafo';
+import Parrafo1 from './components/atomos/Parrafo1';
+import Botons from './components/atomos/Botons';
+import Image from './components/atomos/Image';
+import List from './components/atomos/List';
+import Image1 from './components/atomos/Image1';
+import Organize from './components/atomos/Organize';
+import Imagenes2 from './components/atomos/Imagenes2';
 
 
 

fd361198846fcb552a4362b6e2a031fc0e9e9dc9
raulenzo <raru69@hotmail.com>
2023-07-04 09:42:45 +0200

    atomos

--- src/components/atomos/Image.jsx
+++ src/components/atomos/Image.jsx
@@ -1,15 +1,17 @@
 import './Image-style.css';
+import './Parrafo-style.css';
 
 const Image = () => {
 const image= "/src/img/description-1-app.png";
     return (
-    
+    <>
     
     <div className= "Image">
     <img src={image} alt="Image 1" width={"500px"} height={"500px"} />
     </div>
     
-
+    
+</>
 
     
   );

d28efe321181347f43cbfe47e8b721414695b56d
AkagamiNoLorea <raiseta92@gmail.com>
2023-07-04 15:09:18 +0200

    en teoria funciona

--- src/components/moleculas/Montaje.jsx
+++ src/components/moleculas/Montaje.jsx
@@ -1,11 +1,11 @@
-import Parrafo0 from './components/atomos/Parrafo0';
-import Parrafo1 from './components/atomos/Parrafo1';
-import Botons from './components/atomos/Botons';
-import Image from './components/atomos/Image';
-import List from './components/atomos/List';
-import Image1 from './components/atomos/Image1';
-import Organize from './components/atomos/Organize';
-import Imagenes2 from './components/atomos/Imagenes2';
+import Parrafo0 from '../atomos/Parrafo0.jsx';
+import Parrafo1 from '../atomos/Parrafo1';
+import Botons from '../atomos/Botons';
+import Image from '../atomos/Image';
+import List from '../atomos/List';
+import Image1 from '../atomos/Image1';
+import Organize from '../atomos/Organize';
+import Imagenes2 from '../atomos/Imagenes2';
 import './Montaje-style.css';
 
 const Montaje = () => {

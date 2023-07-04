73a0c1e6189859f59cd6365a936f6a43066691dd
Albert <alabertus@gmail.com>
2023-07-04 22:53:33 +0200

    icoños

--- src/components/atomos/Icons.jsx
+++ src/components/atomos/Icons.jsx
@@ -1,20 +1,35 @@
 const Icons = () => {
   return (
     <div className="Container">
-      <div className="Icons"><img src="src/img/descarga001.png" width={"85px"} height={"85px"} />
-        <p className="iconsname">Identify Goals</p>
+      <div className="Icons">
+        <img src="src/img/descarga001.png" width={"85px"} height={"85px"} />
       </div>
-      <div className="Icons"><img src="src/img/descarga002.png" width={"85px"} height={"85px"} />
-        <p className="iconsname">Situation Analysis</p>
+      <div className="Icons">
+        <img src="src/img/descarga002.png" width={"85px"} height={"85px"} />
       </div>
-      <div className="Icons"><img src="src/img/descarga003.png" width={"85px"} height={"85px"} />
-        <p className="iconsname">Tasks Setings</p>
+      <div className="Icons">
+        <img src="src/img/descarga003.png" width={"85px"} height={"85px"} />
       </div>
-      <div className="Icons"><img src="src/img/descarga004.png" width={"85px"} height={"85px"} />
-        <p className="iconsname">Social Interaction</p>
+      <div className="Icons">
+        <img src="src/img/descarga004.png" width={"85px"} height={"85px"} />
       </div>
-      <div className="Icons"><img src="src/img/descarga005.png" width={"85px"} height={"85px"} />
-        <p className="iconsname">Get Things Done</p>
+      <div className="iconsname">
+        <p>Identify Goals</p>
+      </div>
+      <div className="iconsname">
+        <p>Situation Analysis</p>
+      </div>
+      <div className="iconsname">
+        <p>Tasks Setings</p>
+      </div>
+      <div className="iconsname">
+        <p>Social Interaction</p>
+      </div>
+      <div className="Icons">
+        <img src="src/img/descarga005.png" width={"85px"} height={"85px"} />
+      </div>
+      <div className="iconsname">
+        <p>Get Things Done</p>
       </div>
     </div>
   );

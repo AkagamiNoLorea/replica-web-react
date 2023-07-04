5f3b301033281ee5d4c5b41c3618f3038ec2a800
Albert <alabertus@gmail.com>
2023-07-04 15:43:02 +0200

    ahoraque pasa

--- src/components/atomos/Icons.jsx
+++ src/components/atomos/Icons.jsx
@@ -1,16 +1,24 @@
 const Icons = () => {
   return (
-    <div className= "Icons">
-      <img src="src/img/descarga001.png" width={"85px"} height={"85px"} />
-      <p className="iconsname">Identify Goals</p>
-      <img src="src/img/descarga002.png" width={"85px"} height={"85px"} />
-      <p className="iconsname">Situation Analysis</p>
-      <img src="src/img/descarga003.png" width={"85px"} height={"85px"} />
-      <p className="iconsname">Tasks Setings</p>
-      <img src="src/img/descarga004.png" width={"85px"} height={"85px"} />
-      <p className="iconsname">Social Interaction</p>
-      <img src="src/img/descarga005.png" width={"85px"} height={"85px"} />
-      <p className="iconsname">Get Things Done</p>
+    <div className="Icons">
+      <div><img src="src/img/descarga001.png" width={"85px"} height={"85px"} />
+        <p className="iconsname">Identify Goals</p>
+      </div>
+      <div><img src="src/img/descarga002.png" width={"85px"} height={"85px"} />
+        <p className="iconsname">Situation Analysis</p>
+      </div>
+      <div><img src="src/img/descarga003.png" width={"85px"} height={"85px"} />
+        <p className="iconsname">Tasks Setings</p>
+      </div>
+      <div><img src="src/img/descarga004.png" width={"85px"} height={"85px"} />
+        <p className="iconsname">Social Interaction</p>
+      </div>
+      <div><img src="src/img/descarga004.png" width={"85px"} height={"85px"} />
+        <p className="iconsname">Social Interaction</p>
+      </div>
+      <div><img src="src/img/descarga005.png" width={"85px"} height={"85px"} />
+        <p className="iconsname">Get Things Done</p>
+      </div>
     </div>
   );
 };

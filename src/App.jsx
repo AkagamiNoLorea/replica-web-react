8831797ecf464dfdc7b0936d44e615936b87235e
Albert <alabertus@gmail.com>
2023-07-04 12:33:46 +0200

    otro desastre

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,10 @@
-import Circuloamarillo from './components/Circuloamarillo';
-import Circuloazul from './components/Circuloazul';
-import Rombo from './components/Rombo';
-import Titulo from './components/Titulo';
-import Boton from './components/Boton';
-import HeaderIphone from './components/HeaderIphone';
-import Icons from './components/Icons';
-
-
+import SecciónMobile from './SeccionMobile.jsx';
 
 function App() {
     return (
         <>
-            <Circuloamarillo />
-            <Circuloazul />
-            <Rombo />
-            <Titulo />
-            <Boton />
-            <HeaderIphone />
-            <Icons />
-        </>
+            <SecciónMobile />
+         </>
     );
 
 }

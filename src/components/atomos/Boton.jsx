5425d1bc29b3523a29be8425ae0265073c042499
Albert <alabertus@gmail.com>
2023-07-04 10:16:03 +0200

    atomos

--- src/components/atomos/Boton.jsx
+++ src/components/atomos/Boton.jsx
@@ -1,7 +1,7 @@
 import './Boton.css'
 const Boton = () => {
   return (
-    <div>
+    <div className= "Boton">
       <img src="src\img\download1.png" />
       <img src="src\img\download2.png" />
     </div>

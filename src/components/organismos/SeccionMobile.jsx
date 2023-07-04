87910c134e77fc3d2d5f01db9652ebbbc83dbc35
AkagamiNoLorea <raiseta92@gmail.com>
2023-07-04 15:33:45 +0200

    en teoria ya se visualiza

--- src/components/organismos/SeccionMobile.jsx
+++ src/components/organismos/SeccionMobile.jsx
@@ -1,12 +1,12 @@
 import './SeccionMobile.css'
 
-import Circuloamarillo from './components/Circuloamarillo';
-import Circuloazul from './components/Circuloazul';
-import Rombo from './components/Rombo';
-import Titulo from './components/Titulo';
-import Boton from './components/Boton';
-import HeaderIphone from './components/HeaderIphone';
-import Icons from './components/Icons';
+import Circuloamarillo from '../atomos/circuloamarillo.jsx';
+import Circuloazul from '../atomos/circuloazul.jsx';
+import Rombo from '../atomos/rombo.jsx';
+import Titulo from '../atomos/Titulo.jsx';
+import Boton from '../atomos/Boton.jsx';
+import HeaderIphone from '../atomos/HeaderIphone.jsx';
+import Icons from '../atomos/Icons.jsx';
 
 const SeccionMobile = () => {
     return (

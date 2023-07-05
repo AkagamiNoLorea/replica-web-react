b25a7fc1084b833a6856dd640820bdde86510163
Akagami <128890496+AkagamiNoLorea@users.noreply.github.com>
2023-07-05 12:06:17 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,9 @@
 import './App.css'
-import Secciontestimonials from './components/organismos/Secciontestimonials';
-import SeccionMobile from './components/organismos/SeccionMobile.jsx';
-import Montaje from './components/moleculas/Montaje';
-
 
 function App() {
     return (
         <>
-            <SeccionMobile />
-            <Montaje/>
-            <Secciontestimonials />
+            
         </>
     );
 }

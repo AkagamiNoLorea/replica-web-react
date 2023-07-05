90632ced797ee0040c99a187eb542ba2c6053e77
AkagamiNoLorea <raiseta92@gmail.com>
2023-07-05 11:38:44 +0200

    arreglando

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import './App.css'
-import Autopubli from './components/plantillas/Autopubli'
+
 
 function App() {
 
   return (
     
-       <div>
-        <Autopubli/>
+        <div>
+    
         </div>
   )
 }

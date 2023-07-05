517f010f8c08203a3ce880361b358dd5a2214da2
AkagamiNoLorea <raiseta92@gmail.com>
2023-07-05 09:58:59 +0200

    harta

---
+++ src/pages/Home.jsx
@@ -0,0 +1,16 @@
+import SeccionMobile from '../components/organismos/SeccionMobile';
+import Montaje from '../components/moleculas/Montaje';
+import Secciontestimonials from '../components/organismos/Secciontestimonials.jsx';
+const Home = () => {
+    return (
+        <>
+        <SeccionMobile/>
+        <Montaje/>
+        <Secciontestimonials/>
+        {/* 
+        <Autopubli/>
+        */}
+        </>
+    )
+}
+export default Home
